f9915d5a20f8d3ffa8efb2adce7877c7afb2ef85
josephdempsey <joeadempsey@gmail.com>
2021-02-24 21:21:26 +0000

    docs(cron): update docs and remove debugger

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 - Install Node v12+ (Currently running `12.14.0`) (Recommend using a node version manager: http://npm.github.io/installation-setup-docs/installing/using-a-node-version-manager.html)
 - Install app run `npm i` from the root directory
+- Build the app run `npm run build` from the root directory
 - Information on cron patterns can found here: http://crontab.org/
 - Basic cron expressions
  * any value
@@ -11,3 +12,8 @@
  - range of values (e.g. 15-30)
  / step values (e.g. */5)
  Any combination of these (e.g. 5-10,20-25,*/15)
+- Running the cron parser `npm run cron -- "1-15 0 1,15 * 1-5 /usr/bin/find"`
+- Example;
+   - ![](./example.png)
+ - Lots of examples located here `./test`
+
--- test/unit/cronExpessionParser.spec.ts
+++ test/unit/cronExpessionParser.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 import { cronExpressionParser } from '../../app/cronExpressionParser';
 import { limits } from '../../app/parseCronValue';
 
@@ -59,7 +60,6 @@ describe('Cron Expression Parser valid fields', () => {
 
 describe('Cron Minute Parser', () => {
   it('should return entire minute range 0-59', async (done) => {
-    debugger;
     const result = cronExpressionParser('* 0 1,15 * 1-5 /usr/bin/find');
     const range: { [ key: number ]: number } = { 1: 1, 5: 5 };
 
@@ -75,7 +75,6 @@ describe('Cron Minute Parser', () => {
   });
 
   it('should return range with 1-15', async (done) => {
-    debugger;
     const result = cronExpressionParser('1-15 0 1,15 * 1-5 /usr/bin/find');
     const range: { [ key: number ]: number } = { };
 
@@ -91,7 +90,6 @@ describe('Cron Minute Parser', () => {
   });
 
   it('should return step value every 20 minutes', async (done) => {
-    debugger;
     const result = cronExpressionParser('*/20 0 1,15 * 1-5 /usr/bin/find');
     const range: { [ key: number ]: number } = { 0: 0, 20: 20, 40: 40 };
 
@@ -103,7 +101,6 @@ describe('Cron Minute Parser', () => {
   });
 
   it('should return comma separated values on 1 & 5 minutes', async (done) => {
-    debugger;
     const result = cronExpressionParser('1,5 0 1,15 * 1-5 /usr/bin/find');
     const range: { [ key: number ]: number } = { 1: 1, 5: 5 };
 
@@ -115,9 +112,10 @@ describe('Cron Minute Parser', () => {
   });
 
   it('should return comma separated ranges between 1-5 & 45-55 & every 20 minutes', async (done) => {
-    debugger;
     const result = cronExpressionParser('1-5,45-55,*/20 0 1,15 * 1-5 /usr/bin/find');
-    const range: { [ key: number ]: number } = { 0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 20: 20, 40: 40, 45: 45, 46: 46, 47: 47, 48: 48, 49: 49, 50: 50, 51: 51, 52: 52, 53: 53, 54: 54, 55: 55 };
+    const range: { [ key: number ]: number } = {
+      0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 20: 20, 40: 40, 45: 45, 46: 46, 47: 47, 48: 48, 49: 49, 50: 50, 51: 51, 52: 52, 53: 53, 54: 54, 55: 55,
+    };
 
     console.log('result', result);
     console.log('range', range);
@@ -129,7 +127,6 @@ describe('Cron Minute Parser', () => {
 
 describe('Cron Hour Parser', () => {
   it('should return entire minute range 0-23', async (done) => {
-    debugger;
     const result = cronExpressionParser('1 * 1,15 * 1-5 /usr/bin/find');
     const range: { [ key: number ]: number } = { };
 
@@ -145,7 +142,6 @@ describe('Cron Hour Parser', () => {
   });
 
   it('should return range with 1-12', async (done) => {
-    debugger;
     const result = cronExpressionParser('1 1-12 1,15 * 1-5 /usr/bin/find');
     const range: { [ key: number ]: number } = { };
 
@@ -161,7 +157,6 @@ describe('Cron Hour Parser', () => {
   });
 
   it('should return step value every 4 hours', async (done) => {
-    debugger;
     const result = cronExpressionParser('1 */4 1,15 * 1-5 /usr/bin/find');
     const range: { [ key: number ]: number } = {
       0: 0, 4: 4, 8: 8, 12: 12, 16: 16, 20: 20,
@@ -175,7 +170,6 @@ describe('Cron Hour Parser', () => {
   });
 
   it('should return comma separated values on 1 & 5 hour', async (done) => {
-    debugger;
     const result = cronExpressionParser('1 1,5 1,15 * 1-5 /usr/bin/find');
     const range: { [ key: number ]: number } = { 1: 1, 5: 5 };
 
@@ -187,7 +181,6 @@ describe('Cron Hour Parser', () => {
   });
 
   it('should return comma separated ranges between 1-5 & 15-20 & every 10 hours', async (done) => {
-    debugger;
     const result = cronExpressionParser('1 1-5,15-20,*/10 1,15 * 1-5 /usr/bin/find');
     const range: { [ key: number ]: number } = {
       0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 10: 10, 15: 15, 16: 16, 17: 17, 18: 18, 19: 19, 20: 20,
@@ -203,7 +196,6 @@ describe('Cron Hour Parser', () => {
 
 describe('Cron Month Parser', () => {
   it('should return entire minute range 1-12', async (done) => {
-    debugger;
     const result = cronExpressionParser('1 1 * * 1-5 /usr/bin/find');
     const range: { [ key: number ]: number } = {};
 
@@ -219,7 +211,6 @@ describe('Cron Month Parser', () => {
   });
 
   it('should return range with 1-10', async (done) => {
-    debugger;
     const result = cronExpressionParser('1 1 1,15 1-10 1-5 /usr/bin/find');
     const range: { [ key: number ]: number } = {};
 
@@ -236,7 +227,6 @@ describe('Cron Month Parser', () => {
 
   // step value on month start with from 1? then go 4, different from min/hour
   it('should return step value every 4 months', async (done) => {
-    debugger;
     const result = cronExpressionParser('1 1 1,15 */4 1-5 /usr/bin/find');
     const range: { [ key: number ]: number } = { 4: 4, 8: 8, 12: 12 };
 
@@ -248,7 +238,6 @@ describe('Cron Month Parser', () => {
   });
 
   it('should return comma separated values on 1 & 5 month', async (done) => {
-    debugger;
     const result = cronExpressionParser('1 1 1,15 1,5 1-5 /usr/bin/find');
     const range: { [ key: number ]: number } = { 1: 1, 5: 5 };
 
@@ -260,7 +249,6 @@ describe('Cron Month Parser', () => {
   });
 
   it('should return comma separated ranges between 1-5 & 10-12 & every 4 month', async (done) => {
-    debugger;
     const result = cronExpressionParser('1 1 1,15 1-5,10-12,*/4 1-5 /usr/bin/find');
     const range: { [ key: number ]: number } = {
       1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 8: 8, 10: 10, 11: 11, 12: 12,
@@ -276,7 +264,6 @@ describe('Cron Month Parser', () => {
 
 describe('Cron Day of the Month Parser', () => {
   it('should return entire minute range 1-31', async (done) => {
-    debugger;
     const result = cronExpressionParser('1 1 * * 1-5 /usr/bin/find');
     const range: { [ key: number ]: number } = {};
 
@@ -292,7 +279,6 @@ describe('Cron Day of the Month Parser', () => {
   });
 
   it('should return range with 1-15', async (done) => {
-    debugger;
     const result = cronExpressionParser('1 1 1-15 1-10 1-5 /usr/bin/find');
 
     const range: { [ key: number ]: number } = {};
@@ -310,7 +296,6 @@ describe('Cron Day of the Month Parser', () => {
 
   // step value on month start with from 1? then go 4, different from min/hour
   it('should return step value every 10 days', async (done) => {
-    debugger;
     const result = cronExpressionParser('1 1 */10 */4 1-5 /usr/bin/find');
     const range: { [ key: number ]: number } = { 10: 10, 20: 20, 30: 30 };
 
@@ -322,7 +307,6 @@ describe('Cron Day of the Month Parser', () => {
   });
 
   it('should return comma separated values on 1st & 15th day', async (done) => {
-    debugger;
     const result = cronExpressionParser('1 1 1,15 1,5 1-5 /usr/bin/find');
     const range: { [ key: number ]: number } = { 1: 1, 15: 15 };
 
@@ -334,7 +318,6 @@ describe('Cron Day of the Month Parser', () => {
   });
 
   it('should return comma separated ranges between 1-5 & 10-12 & every 10 day', async (done) => {
-    debugger;
     const result = cronExpressionParser('* * 1-5,10-12,*/10 * * /usr/bin/find');
     const range: { [ key: number ]: number } = {
       1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 10: 10, 11: 11, 12: 12, 20: 20, 30: 30,
@@ -350,7 +333,6 @@ describe('Cron Day of the Month Parser', () => {
 
 describe('Cron Day of the Week Parser', () => {
   it('should return entire minute range 1-7', async (done) => {
-    debugger;
     const result = cronExpressionParser('1 1 1 1 * /usr/bin/find');
     const range: { [ key: number ]: number } = {};
 
@@ -366,7 +348,6 @@ describe('Cron Day of the Week Parser', () => {
   });
 
   it('should return range with 1-5', async (done) => {
-    debugger;
     const result = cronExpressionParser('1 1 1 1 1-5 /usr/bin/find');
     const range: { [ key: number ]: number } = {};
 
@@ -383,7 +364,6 @@ describe('Cron Day of the Week Parser', () => {
 
   // step value on month start with from 1? then go 4, different from min/hour
   it('should return step value every 2 days', async (done) => {
-    debugger;
     const result = cronExpressionParser('1 1 1 1 */2 /usr/bin/find');
     const range: { [ key: number ]: number } = { 2: 2, 4: 4, 6: 6 };
 
@@ -395,7 +375,6 @@ describe('Cron Day of the Week Parser', () => {
   });
 
   it('should return comma separated values on 1st & 4th day', async (done) => {
-    debugger;
     const result = cronExpressionParser('1 1 1 1 1,4 /usr/bin/find');
     const range: { [ key: number ]: number } = { 1: 1, 4: 4 };
 
@@ -407,7 +386,6 @@ describe('Cron Day of the Week Parser', () => {
   });
 
   it('should return comma separated ranges between 1-2 & 4-5 & every 2 days', async (done) => {
-    debugger;
     const result = cronExpressionParser('1 1 1 1 1-2,4-5,*/2 /usr/bin/find');
     const range: { [ key: number ]: number } = {
       1: 1, 2: 2, 4: 4, 5: 5, 6: 6,
