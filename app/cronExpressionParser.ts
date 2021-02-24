2460142b93064bac9984eb032f84d04a3246ccbe
josephdempsey <joeadempsey@gmail.com>
2021-02-24 21:07:24 +0000

    refactor(remove duplicate code): small refactor to clean up duplicate code

--- app/cronExpressionParser.ts
+++ app/cronExpressionParser.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-continue */
+/* eslint-disable no-plusplus */
 export const limits = {
   minute: {
     min: 0,
@@ -23,6 +25,39 @@ export const limits = {
 
 type TimeMetricType = 'minute'| 'hour'| 'dayOfTheMonth' | 'month' | 'dayOfTheWeek';
 
+const rangeBetween = (value: string, minValue: number, maxValue: number, minuteResult: { [key: number]: number; }) => {
+  const range = value.split('-');
+  const parsedMinRange = parseInt(range[0], 10);
+  const parsedMaxRange = parseInt(range[1], 10);
+
+  if ((parsedMinRange < minValue || parsedMinRange > maxValue)
+    || (parsedMaxRange < minValue || parsedMaxRange > maxValue)) {
+    throw new Error(`Invalid range: ${range}`);
+  }
+
+  for (let i = parsedMinRange; i <= parsedMaxRange; i++) {
+    // eslint-disable-next-line no-param-reassign
+    minuteResult[i] = i;
+  }
+};
+
+const stepValues = (value: string, minValue: number, maxValue: number, minuteResult: { [key: number]: number; }) => {
+  const stepValue = value.split('/');
+
+  const parsedStepValue = parseInt(stepValue[1], 10);
+
+  if (stepValue[0] !== '*' && !Number.isNaN(parsedStepValue) && Math.sign(parsedStepValue) !== -1) {
+    throw new Error(`Invalid step value: ${stepValue}`);
+  }
+
+  for (let i = minValue; i <= maxValue; i++) {
+    if (i % parsedStepValue === 0) {
+      // eslint-disable-next-line no-param-reassign
+      minuteResult[i] = i;
+    }
+  }
+};
+
 const parseCronValue = (cronValue: string, timeMetric: TimeMetricType) => {
   const minuteResult: { [ key: number ]: number } = {};
   const minValue = limits[timeMetric].min;
@@ -40,36 +75,13 @@ const parseCronValue = (cronValue: string, timeMetric: TimeMetricType) => {
 
   if (values.length === 1) {
     if (values[0].includes('-')) {
-      const range = values[0].split('-');
-      const parsedMinRange = parseInt(range[0], 10);
-      const parsedMaxRange = parseInt(range[1], 10);
-
-      if ((parsedMinRange < minValue || parsedMinRange > maxValue)
-                || (parsedMaxRange < minValue || parsedMaxRange > maxValue)) {
-        throw new Error(`Invalid range: ${range}`);
-      }
-
-      for (let i = parsedMinRange; i <= parsedMaxRange; i++) {
-        minuteResult[i] = i;
-      }
+      rangeBetween(values[0], minValue, maxValue, minuteResult);
 
       return minuteResult;
     }
 
     if (values[0].includes('/')) {
-      const stepValue = values[0].split('/');
-
-      const parsedStepValue = parseInt(stepValue[1], 10);
-
-      if (stepValue[0] !== '*' && !Number.isNaN(parsedStepValue) && Math.sign(parsedStepValue) !== -1) {
-        throw new Error(`Invalid step value: ${stepValue}`);
-      }
-
-      for (let i = minValue; i <= maxValue; i++) {
-        if (i % parsedStepValue === 0) {
-          minuteResult[i] = i;
-        }
-      }
+      stepValues(values[0], minValue, maxValue, minuteResult);
 
       return minuteResult;
     }
@@ -77,39 +89,14 @@ const parseCronValue = (cronValue: string, timeMetric: TimeMetricType) => {
 
   if (values.length > 1) {
     for (let i = 0; i < values.length; i++) {
-      // refactor to remove duplicates
       if (values[i].includes('-')) {
-        const range = values[i].split('-');
-        const parsedMinRange = parseInt(range[0], 10);
-        const parsedMaxRange = parseInt(range[1], 10);
-
-        if ((parsedMinRange < minValue || parsedMinRange > maxValue)
-                    || (parsedMaxRange < minValue || parsedMaxRange > maxValue)) {
-          throw new Error(`Invalid range: ${range}`);
-        }
-
-        for (let i = parsedMinRange; i <= parsedMaxRange; i++) {
-          minuteResult[i] = i;
-        }
+        rangeBetween(values[i], minValue, maxValue, minuteResult);
 
         continue;
       }
 
-      // refactor array to object to remove duplicates
       if (values[i].includes('/')) {
-        const stepValue = values[i].split('/');
-
-        const parsedStepValue = parseInt(stepValue[1], 10);
-
-        if (stepValue[0] !== '*' && !Number.isNaN(parsedStepValue) && Math.sign(parsedStepValue) !== -1) {
-          throw new Error(`Invalid step value: ${stepValue}`);
-        }
-
-        for (let i = minValue; i <= maxValue; i++) {
-          if (i % parsedStepValue === 0) {
-            minuteResult[i] = i;
-          }
-        }
+        stepValues(values[i], minValue, maxValue, minuteResult);
 
         continue;
       }
@@ -117,8 +104,9 @@ const parseCronValue = (cronValue: string, timeMetric: TimeMetricType) => {
       const parsedMinute = parseInt(values[i], 10);
 
       if (!Number.isNaN(parsedMinute) && Math.sign(parsedMinute) !== -1) {
-            minuteResult[parsedMinute] = parsedMinute;
-            continue;
+        minuteResult[parsedMinute] = parsedMinute;
+
+        continue;
       }
       throw new Error(`Invalid ${timeMetric} value: ${parsedMinute}`);
     }
@@ -129,7 +117,9 @@ const parseCronValue = (cronValue: string, timeMetric: TimeMetricType) => {
   const parsedMinute = parseInt(cronValue, 10);
 
   if (!Number.isNaN(parsedMinute) && Math.sign(parsedMinute) !== -1) {
-    return minuteResult[parsedMinute] = parsedMinute;
+    minuteResult[parsedMinute] = parsedMinute;
+
+    return minuteResult;
   }
   throw new Error(`Invalid ${timeMetric} value: ${parsedMinute}`);
 };
